46dfc54b724326954c797b0923aeeba0b4ba921b
Lawren E <59849750+lelderki@users.noreply.github.com>
2020-02-19 16:30:20 -0500

    Update Tile.gd

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -1,6 +1,9 @@
 extends StaticBody2D
 
 var points = 10
+onready var _target = position
 
 func _ready():
-	pass
+	position.y = -30
+	$Tween.interpolate_property (self, "positon", position, _target, 2.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+	$Tween.start()
